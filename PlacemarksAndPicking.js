c25f0dea10a229ed941a214639cddafc6de415d9
niharikakhopade <91284222+niharikakhopade@users.noreply.github.com>
2021-10-02 22:48:48 +0530

    All debris added
    Placemarks for all debris added

--- PlacemarksAndPicking.js
+++ PlacemarksAndPicking.js
@@ -1,11 +1,37 @@
 /**
  * Created by Niharika Ganesh 26 Nov 2021
  */
-var DatePicked = new Date();
+// var DatePicked = new Date();
+var DatePicked;
+var fileContentArray=[];
+var TLEFileSelected;
+ function redrawOnDemand(dt){
+    DatePicked=document.getElementById("debrisDT").value;
+    DatePicked=String(DatePicked);
+    DatePicked= new Date(DatePicked);
+}
 
-//  function redrawOnDemand(dt){
-//     DatePicked=document.getElementById("debrisDT").value;
-// }
+function previewFile() {
+    const preview = document.querySelector('txt');
+    const file = document.querySelector('input[type=file]').files[0];
+    const reader = new FileReader();
+  
+    reader.addEventListener("load", function () {
+        fileContentArray = reader.result.split(/\r\n|\n/);
+        TLEFileSelected=true;
+        // readTLEDataFile();
+    //     for(var line = 0; line <= fileContentArray.length-1; line++){
+    //          console.log(line + " --> " + fileContentArray[line]);
+    //   }
+    }, false);
+  
+    if (file) {
+      reader.readAsText(file);
+    }
+
+
+  }
+
 requirejs(['./WorldWindShim',
         './LayerManager','./satellite.min','./moment.min.js'],
     function (WorldWind,
@@ -41,7 +67,7 @@ requirejs(['./WorldWindShim',
 
         // Define the images we'll use for the placemarks.
         var images = [
-            "g1.png",
+            "G1.png",
         ];
 
         var pinLibrary = WorldWind.configuration.baseUrl + "images/pushpins/", // location of the image files
@@ -97,6 +123,7 @@ requirejs(['./WorldWindShim',
         var satAttrib = [];
         var satTLE1 = [];
         var satTLE2 = [];
+        var satName = [];
         n=readTLEDataFile();        
         createSatelites(n);
            
@@ -108,13 +135,23 @@ requirejs(['./WorldWindShim',
 
             // Increment the Blue Marble layer's time at a specified frequency.
             timeIndex = ++timeIndex;
+            //calling read TLE and Satellite one more time if the TLE files is selected
+            if (TLEFileSelected) 
+            {
+                TLEFileSelected = false;
+                n=readTLEDataFile();        
+                createSatelites(n);
+                console.log('total ' + n + ' sats  ');
+            }
+
             //placemark = new WorldWind.Placemark(new WorldWind.Position(latitude + timeIndex, longitude , 1e6), true, null);
             for (var i = 0; i < n; i++) {
+            
                 // TLEtoGeo(); 
                 //calling TLE to get geo locations for the selected time
                 TLEtoGeo(sat[i],satTLE1[i],satTLE2[i]); 
                 sat[i].position = new WorldWind.Position(sat[i].latitude , sat[i].longitude, sat[i].height);
-                sat[i].label = "Sat " + i.toString() + "\n"
+                sat[i].label = satName[i] + "\n"
                 + "Lat " + parseInt(sat[i].latitude).toPrecision(4) + "\n"
                 + "Lon " + parseInt(sat[i].longitude).toPrecision(4) + "\n" 
                 + "Height " + parseInt(sat[i].height).toPrecision(4) + "\n" ;
@@ -173,11 +210,16 @@ requirejs(['./WorldWindShim',
             // Initialize a satellite record
             var satrec = satellite.twoline2satrec(tleLine1, tleLine2);
 
-            //  Propagate satellite using time since epoch (in minutes).
-            var positionAndVelocity = satellite.sgp4(satrec, new Date());
+            // //  Propagate satellite using time since epoch (in minutes).
+            // var positionAndVelocity = satellite.sgp4(satrec, new Date());
 
             //  Or you can use a JavaScript Date
-            var positionAndVelocity = satellite.propagate(satrec, new Date());
+            var positionAndVelocity;
+            if (isValidDate (DatePicked)) 
+                positionAndVelocity = satellite.propagate(satrec, DatePicked);
+            else
+                positionAndVelocity = satellite.propagate(satrec, new Date());
+
 
             // The position_velocity result is a key-value pair of ECI coordinates.
             // These are the base results from which all other coordinates are derived.
@@ -196,19 +238,15 @@ requirejs(['./WorldWindShim',
             // You will need GMST for some of the coordinate transforms.
             // http://en.wikipedia.org/wiki/Sidereal_time#Definition
             var gmst;
-            //if (isValidDate (DatePicked)) 
-               // {
-                //DatePicked = moment(DatePicked).format('YYYY-MM-DD HH:MM:SS');
-               // gmst = satellite.gstime(DatePicked);
-               // }
-           // else
+            if (isValidDate (DatePicked)) 
+                gmst = satellite.gstime(DatePicked);
+            else
                 gmst = satellite.gstime(new Date());
             
-            //console.log('Before ' +  DatePicked) // Output: 2020-07-21 07:24:06.
-            
-            //console.log('Moment ' +  DatePicked) // Output: 2020-07-21 07:24:06.
+            console.log('Datepicked  ' +  DatePicked + ' new date ' + new Date()) // Output: 2020-07-21 07:24:06.
+            console.log('gmst ' +  gmst) // Output: 2020-07-21 07:24:06.
 
-            console.log('Date Picked gmst ' + satellite.gstime(DatePicked) + '  Curr date gmst ' + satellite.gstime(new Date()));
+            // console.log('Date Picked gmst ' + satellite.gstime(DatePicked) + '  Curr date gmst ' + satellite.gstime(new Date()));
             // console.log(Date());
                 
             
@@ -253,15 +291,61 @@ requirejs(['./WorldWindShim',
 
 
         function readTLEDataFile(){
-            //something else
-            satTLE1[0] = '1 25544U 98067A   19156.50900463  .00003075  00000-0  59442-4 0  9992',
-            satTLE2[0] = '2 25544  51.6433  59.2583 0008217  16.4489 347.6017 15.51174618173442';    
-            
-            //ISS Zarya
-            satTLE1[1] = '1 25544U 98067A   21274.25816815  .00005249  00000-0  10377-3 0  9993',
-            satTLE2[1] = '2 25544  51.6450 178.2593 0004287  46.8394 104.8068 15.48887264305034';
+            // const reader = new FileReader();
+            // var file = new File(["foo"], "Data_2.txt", {
+            //     type: "text/plain",
+            //   });
 
-            return 2;
+
+            // reader.addEventListener("load", function () {
+            // fileContentArray = reader.result.split(/\r\n|\n/);
+            //     }, false);
+  
+            // if (file) {
+            //     reader.readAsText(file);
+            // }
+  
+    
+
+            if (fileContentArray.length <= 0) 
+            {
+                //if no file chose show two satellites             
+                
+                satName[0] = 'Debris X'
+                satTLE1[0] = '1 25544U 98067A   19156.50900463  .00003075  00000-0  59442-4 0  9992',
+                satTLE2[0] = '2 25544  51.6433  59.2583 0008217  16.4489 347.6017 15.51174618173442';    
+                
+                //ISS Zarya
+                satName[1] = 'ISS Zarya'
+                satTLE1[1] = '1 25544U 98067A   21274.25816815  .00005249  00000-0  10377-3 0  9993',
+                satTLE2[1] = '2 25544  51.6450 178.2593 0004287  46.8394 104.8068 15.48887264305034';
+
+                return 2;
+            }
+            else
+            {
+
+                var satCounter = 0;
+                var line = 0;
+                while (line <= fileContentArray.length-1)
+                {
+                    
+                    //console.log(line + " --> " + fileContentArray[line]);
+                    satName[satCounter] = fileContentArray[line];
+                    satTLE1[satCounter] = fileContentArray[line+1];
+                    satTLE2[satCounter] = fileContentArray[line+2];    
+                    satCounter++;
+                    line= line + 3;
+                }
+                return satCounter;
+
+            }    
+
+                
+
+                 
+              
+
         }         
           // Run the animation at the desired frequency.
           window.setInterval(animateTimeSeries, animationStep);
